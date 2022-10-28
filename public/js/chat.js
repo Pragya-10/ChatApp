2a55ec2422fafe29f4e1e76d0bcdc78880da793b
Pragya <pragyamishra10501@gmail.com>
2022-10-28 16:19:30 +0530

    Third Commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,27 @@
 const socket = io();
 
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+
 socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   const message = e.target.elements.message.value;
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) => {
+    // console.log("The message was delivered!", message);
+
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message Delivered");
+  });
 });
 
 document.querySelector("#send-location").addEventListener("click", () => {
@@ -18,9 +31,15 @@ document.querySelector("#send-location").addEventListener("click", () => {
 
   navigator.geolocation.getCurrentPosition((position) => {
     // console.log(position);
-    socket.emit("sendLocation", {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log("Location Shared!");
+      }
+    );
   });
 });
