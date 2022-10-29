1299c06cf2b56e5ca41f9def842dab674478a22c
Pragya <pragyamishra10501@gmail.com>
2022-10-30 03:58:16 +0530

    Fourth Commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,6 +3,7 @@ const socket = io();
 const $messageForm = document.querySelector("#message-form");
 const $messageFormInput = $messageForm.querySelector("input");
 const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
 
 socket.on("message", (message) => {
   console.log(message);
@@ -15,7 +16,9 @@ $messageForm.addEventListener("submit", (e) => {
 
   const message = e.target.elements.message.value;
   socket.emit("sendMessage", message, (error) => {
-    // console.log("The message was delivered!", message);
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
 
     if (error) {
       return console.log(error);
@@ -24,10 +27,12 @@ $messageForm.addEventListener("submit", (e) => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser ");
   }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
 
   navigator.geolocation.getCurrentPosition((position) => {
     // console.log(position);
@@ -38,6 +43,7 @@ document.querySelector("#send-location").addEventListener("click", () => {
         longitude: position.coords.longitude,
       },
       () => {
+        $sendLocationButton.removeAttribute("disabled");
         console.log("Location Shared!");
       }
     );
